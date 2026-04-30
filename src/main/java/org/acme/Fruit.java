fd373c4e2136c1fcf369abcea98ec48fb797cc42
lleonardogr <lleonardogr@gmail.com>
2026-04-30 14:57:55 -0300

    feat: enhance FruitResource and Fruit model with OpenAPI annotations and pagination support

--- src/main/java/org/acme/Fruit.java
+++ src/main/java/org/acme/Fruit.java
@@ -2,10 +2,18 @@ package org.acme;
 
 import io.quarkus.hibernate.orm.panache.Panache;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.smallrye.common.constraint.NotNull;
 import jakarta.persistence.Entity;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @Entity
+@Schema(description = "Uma fruta")
 public class Fruit extends PanacheEntity {
+    @NotNull
+    @Schema(description = "Nome da fruta", examples = {"Maça", "Banana"}, maxLength = 100
+    , required = true)
     public String name;
+    @Schema(description = "Descrição da fruta", examples = {"Fruta de inverno"}
+    , maxLength = 255)
     public String description;
 }
--- src/main/java/org/acme/FruitResource.java
+++ src/main/java/org/acme/FruitResource.java
@@ -1,32 +1,78 @@
 package org.acme;
 
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.ExampleObject;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.resteasy.reactive.RestResponse;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/fruits")
 public class FruitResource {
 
     @GET
-    public List<Fruit> list(){
-        return Fruit.listAll();
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Lista todas as frutas cadastradas.",
+    description = "Retorna um array JSON com todas os registros cadastrados")
+    @Tag(name = "Frutas")
+    @APIResponse(responseCode = "200", description = "Frutas listadas com sucesso",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Fruit.class)
+            ))
+    //@APIResponse(responseCode = "304", description = "Não houve alterações na aplicação desde a ultima requisição") // Se vc tiver cache, é um status code válido
+    public List<Fruit> list(@QueryParam("page") int page){
+        return Fruit.listAll(); // sempre ta procurando no banco
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Busca uma fruta por id", description = "Busca uma fruta por id")
+    @Tag(name = "Frutas")
+    @APIResponse(responseCode = "200", description = "Fruta encontrada com sucesso",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Fruit.class)
+            ))
+    @APIResponse(responseCode = "404", description = "Fruta não encontrada")
+    public Response getFruitById(
+            @Parameter(description = "ID da fruta", required = true, examples = {@ExampleObject(name = "ID da fruta", value = "1")})
+            @PathParam("id") Long id){
+        return  Fruit.findByIdOptional(id)
+                .map(f -> Response.ok().entity(f).build())
+                .orElse(Response.status(Response.Status.NOT_FOUND).build());
     }
 
     @POST
     @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponse(responseCode = "201", description = "Fruta cadastrada com sucesso",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Fruit.class)
+            ))
+    @APIResponse(responseCode = "400", description = "Requisição inválida")
     public Response add(Fruit fruit){
-        fruit.persist();
+        Fruit.persist(fruit);
         return Response.created(URI.create("/fruits/"+fruit.id)).entity(fruit).build();
     }
 
     @DELETE
     @Transactional
+    @APIResponse(responseCode = "404", description = "Fruta não encontrada")
+    @APIResponse(responseCode = "204", description = "Fruta deletada com sucesso")
     public Response delete(Long id){
         Fruit.deleteById(id);
         return Response.noContent().build();
